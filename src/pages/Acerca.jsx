2af978adaf78bcd513d944814bb059afc249b882
Daniel de la Jara <ddelajara@gmail.com>
2023-10-11 23:47:33 -0300

    Subida a Netlify Final

--- src/pages/Acerca.jsx
+++ src/pages/Acerca.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import { Footer } from '../components/Footer';
 
 export const Acerca = () => {
     return (
-        <div className="container mt-5">
+    <>    
+        <Container>
+        <div className="mt-5">
             {/* Espacio para la imagen */}
             <div className="d-flex justify-content-center mb-4">
                 <Image src="/image/acerca.jpg" alt="Acerca de nosotros" />
@@ -14,5 +18,8 @@ export const Acerca = () => {
                 <h1>Somos, Rock, Arte y Estilo</h1>
             </div>
         </div>
+        </Container>
+        <Footer />
+    </>
     );
 };
